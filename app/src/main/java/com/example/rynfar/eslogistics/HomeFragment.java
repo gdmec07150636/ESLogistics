7fc79b1f97498a62e2c12fcbe9ddec2887ec05d0
yu07150649 <2219466903@qq.com>
2017-05-16 21:04:53 +0800

    主页更新

--- app/src/main/java/com/example/rynfar/eslogistics/HomeFragment.java
+++ app/src/main/java/com/example/rynfar/eslogistics/HomeFragment.java
@@ -3,9 +3,17 @@ package com.example.rynfar.eslogistics;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class HomeFragment extends Fragment {
@@ -25,7 +33,24 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+        ListView listView = (ListView) v.findViewById(R.id.home_list);
+        List<Map<String, Object>> list = new ArrayList<>();
+        initData(list);
+        String[] from = {"head", "title", "summary"};
+        int[] to = new int[]{R.id.message_head, R.id.message_title, R.id.message_summary};
+        SimpleAdapter adapter = new SimpleAdapter(getContext(), list, R.layout.message_item, from, to);
+        listView.setAdapter(adapter);
         return v;
     }
+    private void initData(List<Map<String, Object>> list) {
+        for (int i = 0; i < 100; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("head", R.mipmap.ic_favorite_white_24dp);
+            map.put("title", "标题" + i);
+            map.put("summary", "内容摘要" + i);
+            list.add(map);
+        }
+    }
+
 
 }
