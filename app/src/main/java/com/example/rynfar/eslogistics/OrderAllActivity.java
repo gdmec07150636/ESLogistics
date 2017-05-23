22149129d7b44be13be921d8ae4dccd33c89cf48
yu07150649 <2219466903@qq.com>
2017-05-23 17:02:06 +0800

    全部订单页

---
+++ app/src/main/java/com/example/rynfar/eslogistics/OrderAllActivity.java
@@ -0,0 +1,25 @@
+package com.example.rynfar.eslogistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class OrderAllActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_all);
+        toolbar = (Toolbar) findViewById(R.id.order_all_toolbar);
+        toolbar.setTitle("我的订单");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
